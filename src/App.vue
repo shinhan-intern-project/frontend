7631227b34039e81eecae81022300813c62e3c3d
ZZZINU <nnjin987@gmail.com>
2025-05-10 12:19:54 +0900

    P4-33(#6) [SETTING]: global css 적용

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,5 @@
 <template>
-  <div id="app">
-    <!-- 네비게이션 메뉴 -->
-    <nav>
-      <router-link to="/">홈</router-link> |
-      <router-link to="/stocks">종목</router-link> |
-      <router-link to="/commodities">품목</router-link>
-    </nav>
-    
-    <!-- 라우터 뷰 -->
     <router-view/>
-  </div>
 </template>
 
 <script>
@@ -19,27 +9,47 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+html, body, div, span, applet, object, iframe,
+h1, h2, h3, h4, h5, h6, p, blockquote, pre,
+a, abbr, acronym, address, big, cite, code,
+del, dfn, em, img, ins, kbd, q, s, samp,
+small, strike, strong, sub, sup, tt, var,
+b, u, i, center,
+dl, dt, dd, ol, ul, li,
+fieldset, form, label, legend,
+table, caption, tbody, tfoot, thead, tr, th, td,
+article, aside, canvas, details, embed, 
+figure, figcaption, footer, header, hgroup, 
+menu, nav, output, ruby, section, summary,
+time, mark, audio, video {
+	margin: 0;
+	padding: 0;
+	border: 0;
+	font-size: 100%;
+	font: inherit;
+	vertical-align: baseline;
 }
-
-nav {
-  padding: 30px;
+/* HTML5 display-role reset for older browsers */
+article, aside, details, figcaption, figure, 
+footer, header, hgroup, menu, nav, section {
+	display: block;
 }
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-  text-decoration: none;
-  margin: 0 10px;
+body {
+	line-height: 1;
 }
-
-nav a.router-link-exact-active {
-  color: #42b983;
+ol, ul {
+	list-style: none;
+}
+blockquote, q {
+	quotes: none;
+}
+blockquote:before, blockquote:after,
+q:before, q:after {
+	content: '';
+	content: none;
+}
+table {
+	border-collapse: collapse;
+	border-spacing: 0;
 }
 </style>
