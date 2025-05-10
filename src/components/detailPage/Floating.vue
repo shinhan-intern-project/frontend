04ec2ea13c9c3b1aa323de29a895aae1480a096a
ZZZINU <nnjin987@gmail.com>
2025-05-10 15:50:04 +0900

    P4-32(#5) [DESIGN]: 디테일 페이지 플로팅 목차 생성

---
+++ src/components/detailPage/Floating.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="floating-container">
+    <div class="floating-item active">주식 개요</div>
+    <div class="floating-item">네트워크 그래프</div>
+    <div class="floating-item">관련 품목</div>
+    <div class="floating-item">캔들 차트</div>
+    <div class="floating-item">관련 뉴스</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "DetailFloating",
+};
+</script>
+
+<style scoped>
+.floating-container {
+  position: sticky;
+  top: 120px;
+  width: 160px;
+  display: flex;
+  flex-direction: column;
+  gap: 8px;
+  align-self: flex-start;
+}
+
+.floating-item {
+  display: flex;
+  justify-content: start;
+  align-items: center;
+  width: 160px;
+  height: 40px;
+  box-sizing: border-box;
+  padding: 0px 20px;
+  border-radius: 30px;
+  background: #fff;
+  box-shadow: 0px 4px 15px 0px rgba(58, 112, 191, 0.1);
+
+  color: rgba(0, 12, 55, 0.3);
+  font-weight: 700;
+}
+
+.floating-item.active {
+  color: #000c37;
+}
+</style>
--- src/components/detailPage/Layout.vue
+++ src/components/detailPage/Layout.vue
@@ -2,30 +2,45 @@
   <div class="detail-layout-container">
     <div class="detail-layout-content">
       <DetailHeader />
+      <div class="detail-layout-content-container">
+        <DetailFloating />
+        <div class="test">dd</div>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
 import DetailHeader from "./Header.vue";
+import DetailFloating from "./Floating.vue";
 
 export default {
   name: "DetailLayout",
   components: {
     DetailHeader,
+    DetailFloating,
   },
 };
 </script>
 
 <style scoped>
 .detail-layout-container {
-  margin: 100px 0px;
+  padding: 100px 0px;
   width: 100%;
-  height: 100vh;
+  box-sizing: border-box;
 }
 
 .detail-layout-content {
   max-width: 1200px;
   margin: 0 auto;
+}
+
+.detail-layout-content-container {
+  display: flex;
+  margin-top: 100px;
+}
+
+.test {
+  height: 2000px;
 }
 </style>
